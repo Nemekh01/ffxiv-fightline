24de223fd1d4583d7f05ecc2294900e73519c437
SKachkovsky <airex.s@gmail.com>
2019-05-10 20:51:05 +0300

    context menu experiment

--- FightTimeLine/ClientApp/src/fightline/contextmenu/contextmenu.component.ts
+++ FightTimeLine/ClientApp/src/fightline/contextmenu/contextmenu.component.ts
@@ -1,7 +1,7 @@
-import { Component, Inject, EventEmitter, ViewChild, ViewChildren, Output, QueryList, HostListener } from "@angular/core";
-import { ContextMenuService, ContextMenuComponent } from "ngx-contextmenu"
+import { Component, Inject, EventEmitter, ViewChild, ViewChildren, Output, QueryList, HostListener, TemplateRef } from "@angular/core";
+import { NzDropdownContextComponent, NzDropdownService, NzMenuItemDirective } from 'ng-zorro-antd';
 import { IAbilityFilter, IContextMenuData } from "../../core/Models"
-import * as Sentry from "@sentry/browser";
+
 
 
 @Component({
@@ -11,31 +11,26 @@ import * as Sentry from "@sentry/browser";
 })
 export class FightLineContextMenuComponent {
 
-  @ViewChild("contextMenu")
-  contextMenu: ContextMenuComponent;
-  @ViewChildren(ContextMenuComponent)
-  downtimeMenus: QueryList<ContextMenuComponent>;
+  private dropdown: NzDropdownContextComponent;
 
+  @ViewChild("contextMenu")
+  contextMenu: TemplateRef<any>;
+  
   @Output("filterUpdated") filterUpdated: EventEmitter<IAbilityFilter> = new EventEmitter<IAbilityFilter>();
 
 
-  jobFilter: IAbilityFilter;
+  //jobFilter: IAbilityFilter;
   contextMenuActions: IContextMenuData[] = [];
-  pets: any[];
-  hidden: any[];
+//  pets: any[];
+//  hidden: any[];
   private lastDoubleClickEvent: any;
 
   setLastDoubleClick(e: any): void {
-//    const data = JSON.stringify(this.serializeEvent(event));
-//
-//    alert(data);
-   
     if (event && e.clientX)
       this.lastDoubleClickEvent = e;
   }
 
-  constructor(private contextMenuService: ContextMenuService) {
-
+  constructor(private nzDropdownService: NzDropdownService) {
   }
 
 
@@ -50,65 +45,17 @@ export class FightLineContextMenuComponent {
     if (event && event.clientX)
       this.lastDoubleClickEvent = event;
   }
-
-  serializeEvent (e) {
-    if (e) {
-      const o = {
-        eventName: e.toString(),
-        altKey: e.altKey,
-        bubbles: e.bubbles,
-        button: e.button,
-        buttons: e.buttons,
-        cancelBubble: e.cancelBubble,
-        cancelable: e.cancelable,
-        clientX: e.clientX,
-        clientY: e.clientY,
-        composed: e.composed,
-        ctrlKey: e.ctrlKey,
-        currentTarget: e.currentTarget ? e.currentTarget.outerHTML : null,
-        defaultPrevented: e.defaultPrevented,
-        detail: e.detail,
-        eventPhase: e.eventPhase,
-        fromElement: e.fromElement ? e.fromElement.outerHTML : null,
-        isTrusted: e.isTrusted,
-        layerX: e.layerX,
-        layerY: e.layerY,
-        metaKey: e.metaKey,
-        movementX: e.movementX,
-        movementY: e.movementY,
-        offsetX: e.offsetX,
-        offsetY: e.offsetY,
-        pageX: e.pageX,
-        pageY: e.pageY,
-        relatedTarget: e.relatedTarget ? e.relatedTarget.outerHTML : null,
-        returnValue: e.returnValue,
-        screenX: e.screenX,
-        screenY: e.screenY,
-        shiftKey: e.shiftKey,
-        sourceCapabilities: e.sourceCapabilities ? e.sourceCapabilities.toString() : null,
-        target: e.target ? e.target.outerHTML : null,
-        timeStamp: e.timeStamp,
-        toElement: e.toElement ? e.toElement.outerHTML : null,
-        type: e.type,
-        view: e.view ? e.view.toString() : null,
-        which: e.which,
-        x: e.x,
-        y: e.y
-      };
-
-      return JSON.stringify(o, null, 2);
-    }
-    return null;
-  };
+  
 
   public openStanceSelector(data: IContextMenuData[]): void {
     this.contextMenuActions = data;
     setTimeout(() => {
-      this.contextMenuService.show.next({
-        contextMenu: this.contextMenu,
-        event: this.lastDoubleClickEvent,
-        item: "",
-      });
+      //todo: stance selector
+//      this.contextMenuService.show.next({
+//        contextMenu: this.contextMenu,
+//        event: this.lastDoubleClickEvent,
+//        item: "",
+//      });
     });
   }
 
@@ -118,61 +65,63 @@ export class FightLineContextMenuComponent {
 
     if (items == null || items.length === 0) return;
 
-    const filterItem = items.find((it: IContextMenuData) => !!it.filter);
-    if (filterItem)
-      this.jobFilter = filterItem.filter;
-
-    const petItem = items.find((it: IContextMenuData) => !!it.pets);
-    if (petItem)
-      this.pets = petItem.pets;
-
-    const hiddenItem = items.find((it: IContextMenuData) => !!it.hidden);
-    if (hiddenItem)
-      this.hidden = hiddenItem.hidden;
+//    const filterItem = items.find((it: IContextMenuData) => !!it.filter);
+//    if (filterItem)
+//      this.jobFilter = filterItem.filter;
+//
+//    const petItem = items.find((it: IContextMenuData) => !!it.pets);
+//    if (petItem)
+//      this.pets = petItem.pets;
+//
+//    const hiddenItem = items.find((it: IContextMenuData) => !!it.hidden);
+//    if (hiddenItem)
+//      this.hidden = hiddenItem.hidden;
 
     this.contextMenuActions = items;
     setTimeout(() => {
-      this.contextMenuService.show.next({
-        contextMenu: this.contextMenu,
-        event: event.event,
-        item: event.item,
-
-      });
+      this.dropdown = this.nzDropdownService.create(event.event, this.contextMenu);
     });
   }
 
-  selectSubmenu(action: any, filterMenu: any, petsMenu: any, hiddenMenu: any) {
+  selectSubmenu(action: any) {
     if (!action) return null;
-    if (action.filter) {
-      return filterMenu;
-    }
+    if (action.filter)
+      return "filter";
     if (action.isDowntime)
-      return this.downtimeMenus.find(it => it.menuClass.indexOf(action.item.id) >= 0) || { mock: true };
+      return "downtime";
     if (action.pets)
-      return petsMenu;
+      return "pets";
     if (action.hidden)
-      return hiddenMenu;
-    return null;
+      return "hidden";
+    return 'simple';
   }
 
-  resetJobFilter() {
+  resetJobFilter(filter: IAbilityFilter) {
     console.log("reset job filter requested");
-    this.jobFilter = {
-      unused: undefined,
-      utility: undefined,
-      damage: undefined,
-      selfDefence: undefined,
-      partyDefence: undefined,
-      healing: undefined,
-      healingBuff: undefined,
-      partyDamageBuff: undefined,
-      selfDamageBuff: undefined
-    };
-    this.filterUpdated.emit(this.jobFilter);
+    Object.assign(filter,
+      {
+        unused: undefined,
+        utility: undefined,
+        damage: undefined,
+        selfDefence: undefined,
+        partyDefence: undefined,
+        healing: undefined,
+        healingBuff: undefined,
+        partyDamageBuff: undefined,
+        selfDamageBuff: undefined
+      });
+    this.filterUpdated.emit();
+  }
+
+  close(action: any) {
+    action();
+    this.dropdown.close();
   }
 
   updateFilter(data: IAbilityFilter): void {
-    this.filterUpdated.emit(data);
+    setTimeout(() => {
+      this.filterUpdated.emit();
+    });
   }
 
 }
