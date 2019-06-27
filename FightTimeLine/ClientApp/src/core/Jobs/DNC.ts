f20e747b7d2bddfbf80ba6d5abee2de1879d437c
SKachkovsky <airex.s@gmail.com>
2019-06-28 01:01:37 +0300

    5.0 job changes

---
+++ FightTimeLine/ClientApp/src/core/Jobs/DNC.ts
@@ -0,0 +1,63 @@
+import { IJob, Role, AbilityType } from "../Models"
+import { settings, getAbilitiesFrom, rangeSharedAbilities, abilitySortFn, medicine } from "./shared"
+
+export const DNC: IJob = {
+  name: "DNC",
+  role: Role.Range,
+  icon: ("JobIcons/"),
+  abilities: [
+    {
+      name: "Shield Samba",
+      duration: 15,
+      cooldown: 120,
+      xivDbId: "",
+      icon: ("Dancer/icon_14"),
+      abilityType: AbilityType.PartyDefense,
+    },
+    {
+      name: "Saber Dance",
+      duration: 15,
+      cooldown: 120,
+      xivDbId: "",
+      icon: ("Dancer/icon_16"),
+      abilityType: AbilityType.SelfDamageBuff,
+    },
+    {
+      name: "Improvisation",
+      duration: 15,
+      cooldown: 180,
+      xivDbId: "",
+      icon: ("Dancer/icon_21"),
+      abilityType: AbilityType.Utility,
+    },
+    {
+      name: "Flourish",
+      duration: 0,
+      cooldown: 60,
+      xivDbId: "",
+      icon: ("Dancer/icon_19"),
+      abilityType: AbilityType.Utility,
+    },
+    {
+      name: "Closed Position",
+      duration: 0,
+      cooldown: 30,
+      xivDbId: "",
+      icon: ("Dancer/icon_15"),
+      abilityType: AbilityType.Utility,
+      settings: [settings.target]
+    },
+    {
+      name: "Curing Waltz",
+      duration: 0,
+      cooldown: 60,
+      xivDbId: "",
+      icon: ("Dancer/icon_13"),
+      abilityType: AbilityType.Healing,
+    },
+    ...getAbilitiesFrom(rangeSharedAbilities),
+    medicine["Dexterity"]
+  ].sort(abilitySortFn),
+};
+
+
---
+++ FightTimeLine/ClientApp/src/core/Jobs/GNB.ts
@@ -0,0 +1,116 @@
+import { IJob, Role, AbilityType } from "../Models"
+import { settings, getAbilitiesFrom, tankSharedAbilities, abilitySortFn, medicine } from "./shared"
+
+export const GNB: IJob = {
+  name: "GNB",
+  role: Role.Tank,
+  icon: ("JobIcons/"),
+  abilities: [
+    {
+      name: "No Mercy",
+      duration: 20,
+      cooldown: 60,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_02"),
+      abilityType: AbilityType.SelfDamageBuff,
+    },
+    {
+      name: "Camouflage",
+      duration: 20,
+      cooldown: 90,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_04"),
+      abilityType: AbilityType.SelfDefense,
+    },
+    {
+      name: "Danger Zone",
+      duration: 0,
+      cooldown: 30,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_08"),
+      abilityType: AbilityType.Damage,
+    },
+    {
+      name: "Nebula",
+      duration: 0,
+      cooldown: 120,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_11"),
+      abilityType: AbilityType.SelfDefense,
+    },
+    {
+      name: "Aurora",
+      duration: 18,
+      cooldown: 60,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_13"),
+      abilityType: AbilityType.Healing,
+      setting: [settings.target]
+    },
+    {
+      name: "Superbolide",
+      duration: 8,
+      cooldown: 420,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_14"),
+      abilityType: AbilityType.SelfDefense,
+    },
+    {
+      name: "Rough Divide",
+      duration: 0,
+      cooldown: 30,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_16"),
+      abilityType: AbilityType.Damage,
+      charges: {
+        count: 2,
+        cooldown: 30
+      }
+    },
+    {
+      name: "Bow Shock",
+      duration: 15,
+      cooldown: 60,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_20"),
+      abilityType: AbilityType.Damage,
+    },
+    {
+      name: "Heart of Light",
+      duration: 15,
+      cooldown: 90,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_21"),
+      abilityType: AbilityType.PartyDefense,
+    },
+    {
+      name: "Heart of Stone",
+      duration: 7,
+      cooldown: 25,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_22"),
+      abilityType: AbilityType.PartyDefense,
+      setting: [settings.target]
+    },
+    {
+      name: "Bloodfest",
+      duration: 0,
+      cooldown: 90,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_25"),
+      abilityType: AbilityType.Utility,
+    },
+    {
+      name: "Blasting Zone",
+      duration: 0,
+      cooldown: 30,
+      xivDbId: "",
+      icon: ("Gunbreaker/icon_26"),
+      abilityType: AbilityType.Damage,
+    },
+    ...getAbilitiesFrom(tankSharedAbilities),
+    medicine["Strength"]
+  ].sort(abilitySortFn),
+};
+
+
