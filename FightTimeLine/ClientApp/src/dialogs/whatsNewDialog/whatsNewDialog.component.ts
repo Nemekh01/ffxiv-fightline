d6e2986a6c8eff155caee35e53a4470d321ebf5f
SKachkovsky <airex.s@gmail.com>
2019-09-29 02:50:46 +0300

    whats new scroll to top on open

--- FightTimeLine/ClientApp/src/dialogs/whatsNewDialog/whatsNewDialog.component.ts
+++ FightTimeLine/ClientApp/src/dialogs/whatsNewDialog/whatsNewDialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, Inject, Input, OnInit, TemplateRef } from "@angular/core";
+import { Component, ViewChild, ElementRef, Inject, Input, OnInit, TemplateRef, AfterViewInit } from "@angular/core";
 import { NzModalRef } from "ng-zorro-antd";
 
 
@@ -8,15 +8,25 @@ import { NzModalRef } from "ng-zorro-antd";
     styleUrls: ["./whatsNewDialog.component.css"]
 })
 
-export class WhatsNewDialog implements OnInit {
+export class WhatsNewDialog implements OnInit, AfterViewInit {
   
   @Input("data") data: any;
   @ViewChild("buttonsTemplate") buttonsTemplate: TemplateRef<any>;
+  @ViewChild("timeline") timeline: ElementRef;
   constructor(
-    private dialogRef: NzModalRef) { }
+    private dialogRef: NzModalRef) {
+    this.dialogRef.afterOpen.subscribe(() => {
+      this.timeline.nativeElement.scrollTop = 0;
+    })
+  }
 
   ngOnInit(): void {
     this.dialogRef.getInstance().nzFooter = this.buttonsTemplate;
+    
+  }
+
+  ngAfterViewInit(): void {
+    
   }
 
 
